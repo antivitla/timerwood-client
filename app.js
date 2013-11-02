f3877e21339a2f4840aa7cf77f26829ca0d6afbc
Виталий Кулешин <antivitla@gmail.com>
2013-11-02 20:56:59 +0400

    initial commit

--- app.js
+++ app.js
@@ -3,46 +3,7 @@
 angular.module("timerApp", ["timerApp.controllers", "timerApp.services"]);
 
 // services
-angular.module("timerApp.services", [])
-
-	// Timer service
-	.service("Timer", function() {
-
-		var self = this;
-
-		// start timer
-		function start() {
-			self.isTicking = true;
-			// debug
-			console.log("Timer started");
-		};
-
-		// stop timer
-		function stop() {
-			self.isTicking = false;
-			// debug
-			console.log("Timer stopped");
-		};
-
-		// (API) toggle timer
-		self.toggle = function() {
-			if(self.isTicking) stop();
-			else start();
-		}
-
-		// (API) status
-		self.isTicking = false;
-	});
+angular.module("timerApp.services", []);
 
 // controllers
-angular.module("timerApp.controllers", [])
-
-	// Timer controller
-	.controller("TimerCtrl",["$scope", "Timer" , function($scope, Timer) {
-		// toggle start/stop
-		$scope.toggle = Timer.toggle;
-		// get status
-		$scope.isTicking = function() { return Timer.isTicking; }
-		// debug
-		console.log("TimerCtrl initialized");
-	}]);
+angular.module("timerApp.controllers", []);
